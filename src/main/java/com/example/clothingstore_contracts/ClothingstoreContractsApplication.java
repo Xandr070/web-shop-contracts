472fb1351a2b0aa4b8cc03608ad6acee14e9d31c
Alexander Ivanov <alexandrivanov7070@gmail.com>
2024-11-25 12:08:52 +0300

    init

---
+++ src/main/java/com/example/clothingstore_contracts/ClothingstoreContractsApplication.java
@@ -0,0 +1,13 @@
+package com.example.clothingstore_contracts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClothingstoreContractsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClothingstoreContractsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/clothingstore_contracts/ClothingstoreContractsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.clothingstore_contracts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClothingstoreContractsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
